e722292170503be9def3715f5ea0d712da42d5c0
odilhon85 <i.o.r@inbox.ru>
2022-07-16 16:34:08 +0500

    Changes done first attempt

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int a=number/1000;
+        number-=a*1000;
+        int b=number/100;
+        number-=b*100;
+        int c=number/10;
+        int d=number%10;
+        System.out.println(a+b+c+d);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int a=number/100;
+        number = number-a*100;
+        int b=number/10;
+        int c=number%10;
+        System.out.println(""+c+b+a);
     }
 }
